c5b414dee8934841b7e49c755a70cc58b702f9b1
Wacław Wojcieszko <wojciech.waclawik@gmail.com>
2018-06-20 23:50:57 +0200

    Płotek

--- src/org/wojcieszko/ballOnTheFence/FenceElement.java
+++ src/org/wojcieszko/ballOnTheFence/FenceElement.java
@@ -1,12 +1,37 @@
 package org.wojcieszko.ballOnTheFence;
 
-public class FenceElement {
+public class FenceElement implements Comparable<FenceElement> {
 
     private int x;
+    private int y;
     private int value;
+    private int steps = 0;
 
-    public FenceElement(int x, int value) {
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getSteps() {
+        return steps;
+    }
+
+    public void setSteps(int steps) {
+        this.steps = steps;
+    }
+
+    public void addSteps(int steps) {
+        this.steps += steps;
+    }
+
+
+    public FenceElement(int x, int y, int value) {
         this.x = x;
+        this.y = y;
         this.value = value;
     }
 
@@ -19,11 +44,21 @@ public class FenceElement {
     }
 
     public int getValue() {
+
         return value;
     }
 
     public void setValue(int value) {
+
         this.value = value;
     }
 
+    @Override
+    public int compareTo(FenceElement fenceElement) {
+        if (fenceElement.getValue() > this.getValue()){
+            return -1;
+        } else if (fenceElement.getValue() < this.getValue()){
+            return 1;
+        } else return 0;
+    }
 }
