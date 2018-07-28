daa1a867c0de7e3d7d7ac4959aea9a4806037fcd
Wacław Wojcieszko <wojciech.waclawik@gmail.com>
2018-07-28 19:58:59 +0200

    Lesson180721

--- src/org/wojcieszko/binaryCombinations/BinaryCombinations.java
+++ src/org/wojcieszko/binaryCombinations/BinaryCombinations.java
@@ -5,28 +5,27 @@ public class BinaryCombinations {
 
 //TODO przystosować do wydawania reszty
 
-    public void fun3(int ...data){
+    public void fun3(int... data) {
 
         int combinations = data.length * data.length;
 
-        for(int counter = 0; counter < combinations; ++counter){
+        for (int counter = 0; counter < combinations; ++counter) {
             int temp = counter;
             int[] z = new int[data.length];
 
-            for (int index = 0; index < data.length; ++index){
+            for (int index = 0; index < data.length; ++index) {
                 z[index] = temp % 2;
                 temp = temp / 2;
             }
 
             int value = 0;
             for (int index = 0; index < data.length; ++index) {
-                value += z[index]*data[index];
+                value += z[index] * data[index];
             }
             System.out.println(value);
 
 
         }
-
 
 
     }
