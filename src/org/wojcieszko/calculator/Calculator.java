644bc3ac41df10da7e22053d1fd88dba3e3318de
Wacław Wojcieszko <wojciech.waclawik@gmail.com>
2018-08-12 12:31:45 +0200

    Lesson180805

--- src/org/wojcieszko/calculator/Calculator.java
+++ src/org/wojcieszko/calculator/Calculator.java
@@ -1,10 +1,37 @@
 package org.wojcieszko.calculator;
 
+import java.util.Stack;
+
 public class Calculator {
 
-    public void calculate (String ...data){
+    private Stack<Double> digits = new Stack<>();
+
+    public void operate(String operationInput) {
+
+        String[] operation = operationInput.split(" ");
+
+        for (String element : operation){
+
+            System.out.println(digits.toString());
+
+            System.out.print(" " + element);
+
+            if (element.matches("-?\\d*\\.*\\d*") && !element.equals("-")){
+                digits.push(Double.parseDouble(element));
+//                System.out.println(digits.peek());
+            } else {
+
+                Operator op = Operator.getOp(element);
+
+                if (op == Operator.EQUALS){
+                    System.out.println(digits.peek());
+                } else {
+                    digits.push(Operator.calculate(digits.pop(), digits.pop(), op));
+                }
+
+            }
+
+        }
 
     }
-
-
 }
