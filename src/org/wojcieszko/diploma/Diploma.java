7ffa4d0beae7cf76557b44a6c1dc88d7209b6bee
Wacław Wojcieszko <wojciech.waclawik@gmail.com>
2018-08-18 15:06:33 +0200

    LastHomework

---
+++ src/org/wojcieszko/diploma/Diploma.java
@@ -0,0 +1,17 @@
+package org.wojcieszko.diploma;
+
+public class Diploma {
+    //dawg do scrabble
+    //graf skierowany arraylista w węźle
+    //drzewo po którym się przechodzi
+
+    //poszczególny string "perkoz" rozbijamy na graf krawędź jest literą do węzła który nie jest końcem, krawędzią e przez węzeł krawędzią r itd... aż krawędzią z dojeżdżamy do końca.
+
+    // węzeł: możliwy koniec bądź nie i dostępne krawędzie dla wszystkich liter.
+
+    // czy w tabeli {"perz", "perkoz", "pers", "persja", "pepeg"}
+    // test: "persji", "persj", "pers", "zupa"
+
+    //węzeł ArrayList<Node> lub Map<Character, Node> - lepiej mapa;
+    //hardcorowe przepinanie poddrzew
+}
